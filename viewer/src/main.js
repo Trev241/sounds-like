e962ceabeb4e9f9d4d9ec20a0b4cde0e40e30bea
quarky007 <reddymoramanohar99@gmail.com>
2025-02-27 23:46:52 -0500

    Created react-vite app
    Install the vite and tailwind css on your laptops

---
+++ viewer/src/main.js
@@ -0,0 +1,13 @@
+import "./style.css";
+import viteLogo from "/vite.svg";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+
+    <p class="read-the-docs">
+     hello
+    </p>
+  </div>
+`;
+
+setupCounter(document.querySelector("#counter"));
