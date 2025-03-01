043b7930b0fe1d82cf723a48c7ddf22507b3f357
kushal-7 <chhetrikushal14@gmail.com>
2025-02-28 19:38:00 -0500

    Added Flask Server and Connected to Front-End

--- viewer/src/main.js
+++ viewer/src/main.js
@@ -1,13 +1,22 @@
 import "./style.css";
 import viteLogo from "/vite.svg";
+import axios from "axios";
 
 document.querySelector("#app").innerHTML = `
   <div>
-
     <p class="read-the-docs">
-     hello world
+      hello from frontend react
     </p>
+    <button id="fetchData">Fetch Data from Flask</button>
+    <pre id="response"></pre>
   </div>
 `;
 
-setupCounter(document.querySelector("#counter"));
+document.querySelector("#fetchData").addEventListener("click", async () => {
+  try {
+    const response = await axios.get("http://127.0.0.1:5000/predict");
+    document.querySelector("#response").textContent = JSON.stringify(response.data, null, 2);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+});
