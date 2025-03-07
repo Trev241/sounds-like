7978a7e5b5003077183d5cabbcc07747ad286d5e
Trevis Liu <27953552+Trev241@users.noreply.github.com>
2025-03-06 23:35:35 -0500

    Add authorize route to spotify

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
 import torch
 import sqlite3
+import string
+import random
 
 from learn.models import RecommendationModel
 from fastapi import Request, FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
+from urllib.parse import urlencode
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+print(os.getenv("REDIRECT_URI"))
 
 app = FastAPI()
 
@@ -13,6 +23,23 @@ model.load_state_dict(torch.load("data/recommender.pt", weights_only=False))
 # Opening a database connection
 conn = sqlite3.connect("data/track_metadata.db")
 cursor = conn.cursor()
+
+
+# Getting authorization
+@app.get("/authorize")
+def authorize():
+    query = {
+        "client_id": os.getenv("CLIENT_ID"),
+        "response_type": "token",
+        "redirect_uri": os.getenv("REDIRECT_URI"),
+        "state": "".join(
+            random.SystemRandom().choice(string.ascii_uppercase + string.digits)
+            for _ in range(16)
+        ),
+    }
+
+    query_str = urlencode(query)
+    return RedirectResponse(url=f"https://accounts.spotify.com/authorize?{query_str}")
 
 
 @app.get("/songs")
@@ -65,6 +92,11 @@ def get_song_code(id):
         return int(rows[0][0])
     else:
         raise HTTPException(status_code=400, detail=f"No song with ID - {id}")
+
+
+@app.get("/preview")
+def get_preview(title: str):
+    pass
 
 
 @app.get("/song/{id}")
